c1e40b40ad36fa4b4a88d7d01363b93fd17001f1
Asif Ahmed <asifahmed95420@gmail.com>
2020-05-17 18:15:34 +0530

    login and signin pages created

--- TuneIn/music/views.py
+++ TuneIn/music/views.py
@@ -5,3 +5,9 @@ def index(request):
     return render(request,"music/index.html",{
         "albums": Album.objects.all(),
     })
+
+def login(request):
+    return render(request, "music/login.html")
+
+def signup(request):
+    return render(request,"music/signup.html")
